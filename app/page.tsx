2c7a8bfd08dac6f0f90446cd71b7b8d94d2214f2
Dimkpa Prince <stackninja.pro>
2025-10-30 17:53:59 +0100

    working now

--- app/page.tsx
+++ app/page.tsx
@@ -30,15 +30,12 @@ export default function Home() {
             radius: "full",
             variant: "shadow",
           })}
-          href={siteConfig.links.docs}
         >
           Documentation
         </Link>
         <Link
           isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
+          className={buttonStyles({ variant: "bordered", radius: "full" })}        >
           <GithubIcon size={20} />
           GitHub
         </Link>
